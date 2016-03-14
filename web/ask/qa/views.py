d58607d6d21ee19fd076741d4f4d0548ebd833cf
root <root@admitry77.example.com>
2016-03-14 07:37:47 -0400

    On latest django

--- web/ask/qa/views.py
+++ web/ask/qa/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HtttpResponse
+
+from django.http import HttpResponse 
 def test(request, *args, **kwargs):
-    return HtttpResponse('OK')
+    return HttpResponse('OK')
